38606b547d12cf5fc85338fcd739d4812e026819
ClarenceMock <clarence.sea01@gmail.com>
2022-05-13 23:22:34 +0800

    Add VisionWeb

---
+++ cocos/core/VisionWeb.ts
@@ -0,0 +1,9 @@
+
+export class VisionWeb {
+    static isAddress = (address: string): Boolean => {
+        return true;
+    }
+    static toBigNumber(amount: number) {
+        return 0;
+    }
+}
